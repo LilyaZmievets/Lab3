c862e34ba7df8f9429b1b0d853e3cffdc9b08d84
LilyaZmievets <35195422+LilyaZmievets@users.noreply.github.com>
2018-02-09 11:12:51 +0300

    Update app.py

--- worker/app.py
+++ worker/app.py
@@ -1,21 +1,37 @@
 import redis
 import zmq 
 
-context = zmq.Context()
-socket = context.socket(zmq.REP)
+def Fibbonachi(DataBaseConn):
+    pipe = DataBaseConn.pipeline()
+    while 1:
+            try:
+                print('TRYING')
+                pipe.watch("fibonacciNumber")
+                fibNum1 = int(pipe.lindex("fibonacciNumber", 1))
+                fibNum2 = int(pipe.lindex("fibonacciNumber", 0))
+                sumFib = fibNum1 + fibNum2
+                pipe.lset("fibonacciNumber", 0, sumFib)
+                pipe.lset("fibonacciNumber", 1, fibNum2)
+                pipe.execute()
+                break
+            except redis.WatchError:
+				print('can't calculate')
+                continue
+            finally:
+                pipe.reset()
+    return
+		
+print('BIND')
+Cont = zmq.Context()
+socket = Cont.socket(zmq.REP)
 socket.bind("tcp://*:5555")
 
-def Fibbonachi():
-	Conn = redis.Redis('redis')
-	x2 = Conn.lindex("fib", 1)
-	x1 = Conn.lindex("fib", 0)
-	f1 = int(x1)
-	f2 = int(x2)
-	curFib = f1 + f2
-	strCurFib = str(curFib)
-	return strCurFib
-
+DataBaseConn = redis.Redis('redis')
+print('BINDED')
 while True:
-	mes = socket.recv()
-	curFib = Fibbonachi()
-	socket.send(curFib)
+	poke = socket.recv()
+        print('RECEIVED POKE FROM MASTER')
+	Fibbonachi(DataBaseConn)
+	print('METHOD Fibbonachi executed')
+	socket.send('New Fibonacci number calculated')
+print ('SENT ENDMESSAGE')
